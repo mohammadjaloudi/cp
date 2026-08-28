b1c1626d80fbe9858f088eee18a0268ab4bf43ea
mohammadjaloudi <141223023+mohammadjaloudi@users.noreply.github.com>
2026-08-28 19:38:52 +0300

    Create shoelaceFormula.cpp

---
+++ geometry/shoelaceFormula.cpp
@@ -0,0 +1,76 @@
+// #define _CRT_SECURE_NO_WARNINGS
+#include <bits/stdc++.h>
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+
+// using namespace __gnu_pbds;
+using namespace std;
+
+// #pragma GCC optimize("Ofast")
+// #pragma GCC optimize("unroll-loops")
+// #pragma GCC target("sse,sse2,sse3,ssse3,sse4,lzcnt,popcnt,abm,mmx,avx,avx2,bmi,bmi2")
+
+// template <typename T>
+// using ordered_set =
+//     tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+// template <typename T>
+// using ordered_multiset = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+// ordered_set<int> st
+
+// mt19937_64 rnd(chrono::steady_clock::now().time_since_epoch().count());
+// mt19937 rnd2(chrono::steady_clock::now().time_since_epoch().count());
+
+#define int long long
+#define int64 long long
+#define endl "\n"
+#define pb push_back
+#define all(x) x.begin(), x.end()
+#define print(x) for(auto i : x) cout << i << ' ';
+const double EPS = 1e-9;
+long long MOD = 1e9 + 7;
+long long INF = 1e18;
+long long mod = 998244353;
+int mx = 2e6;
+
+void solve() {
+    int n, h;
+    cin >> n >> h;
+
+    int x = 0, y = 0;
+    int sum = 0;
+    for(int i = 0; i < n; i++) {
+        int s;
+        char c;
+        cin >> s >> c;
+        int nx = x, ny = y;
+        if(c == 'R') nx += s;
+        else if(c == 'L') nx -= s;
+        else if(c == 'U') ny += s;
+        else ny -= s;
+
+        sum += x * ny - y * nx;
+
+        x = nx;
+        y = ny;
+    }
+
+    int area = abs(sum) / 2;
+    int ans = area * h;
+    cout << ans << endl;
+}
+
+int32_t main() {
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    // cout << fixed << setprecision(7);
+
+    int TESTCASES = 1;
+    // cin >> TESTCASES;
+    while (TESTCASES--) solve();
+ 
+    return 0;
+}
