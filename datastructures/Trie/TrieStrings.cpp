c8ed516a5714324a143810ba5aa20db77baa4c65
mohammadjaloudi <141223023+mohammadjaloudi@users.noreply.github.com>
2026-08-29 19:26:38 +0300

    Update TrieStrings.cpp

--- datastructures/Trie/TrieStrings.cpp
+++ datastructures/Trie/TrieStrings.cpp
@@ -1,8 +1,39 @@
+// #define _CRT_SECURE_NO_WARNINGS
 #include <bits/stdc++.h>
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+
+// using namespace __gnu_pbds;
 using namespace std;
 
+// #pragma GCC optimize("Ofast")
+// #pragma GCC optimize("unroll-loops")
+// #pragma GCC target("sse,sse2,sse3,ssse3,sse4,lzcnt,popcnt,abm,mmx,avx,avx2,bmi,bmi2")
+
+// template <typename T>
+// using ordered_set =
+//     tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+// template <typename T>
+// using ordered_multiset = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+// ordered_set<int> st
+
+// mt19937_64 rnd(chrono::steady_clock::now().time_since_epoch().count());
+// mt19937 rnd2(chrono::steady_clock::now().time_since_epoch().count());
+
 #define int long long
-#define endl '\n'
+#define int64 long long
+#define endl "\n"
+#define pb push_back
+#define all(x) x.begin(), x.end()
+#define print(x) for(auto i : x) cout << i << ' ';
+const double EPS = 1e-9;
+long long MOD = 1e9 + 7;
+long long INF = 1e18;
+long long mod = 998244353;
+int mx = 2e6;
+
+map<string, int> mp;
 
 struct Trie {
 
@@ -280,64 +311,42 @@ struct Trie {
 };
 
 
-// =============================================================
-// SOLVE
-// =============================================================
-
 void solve() {
-
     int n;
     cin >> n;
-
     Trie trie;
-
     for(int i = 0; i < n; i++) {
         string s;
         cin >> s;
-
         trie.insert(s);
+        mp[s]++;
     }
 
     int q;
     cin >> q;
-
     while(q--) {
-
         string s;
         cin >> s;
-
-        cout << "Prefix: " << s << endl;
-
-        cout << "Exists: "
-             << trie.startsWith(s) << endl;
-
-        cout << "Count: "
-             << trie.countPrefix(s) << endl;
-
-        cout << "Smallest: "
-             << trie.findSmallest(s) << endl;
-
-        cout << "Largest: "
-             << trie.findLargest(s) << endl;
-
-        cout << "Most frequent: "
-             << trie.maxFreq(s) << endl;
-
-        cout << endl;
+        if(trie.startsWith(s) == 0) cout << -1 << endl;
+        else {
+            string t = trie.maxFreq(s);
+            int cnt = mp[t];
+            cout << t << ' ' << cnt << endl;
+        }
     }
 }
 
-
-// =============================================================
-// MAIN
-// =============================================================
-
 int32_t main() {
-
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
     ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+    cin.tie(0);
 
-    solve();
+    // cout << fixed << setprecision(7);
 
+    int TESTCASES = 1;
+    // cin >> TESTCASES;
+    while (TESTCASES--) solve();
+ 
     return 0;
 }
